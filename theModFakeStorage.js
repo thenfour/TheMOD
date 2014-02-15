75afdb163d5be6cff2d9d382ab2b4dfec92c7427
Carl Corcoran <carlco@gmail.com>
2014-02-15 18:07:26 +0100

    added ogg files, and real content area for about us including scrolling

--- theModFakeStorage.js
+++ theModFakeStorage.js
@@ -16,7 +16,21 @@ var TheModFakeStorage = function()
 	this.pages =
 	[
 		{
-			html: 'jazz / art / music / collaboration / scene',//'scene jazz with a twist, art and music, collaboration of scene artists',
+			html: "<p>The MOD is a jazz fusion quintet performing arrangements of underground "
+			+ "electro jazz/funk. We collaborate with electro jazz artists around the world to "
+			+ "add the human element to the music that deserves it the most. The result is a "
+			+ "blissful combination of catchy melodies, danceable beats, skewed surprises, and"
+			+ " ground-shaking grooves.</p>"
+			+ "<p>Electro-jazz artists since the 90's have been composing ground-breaking music"
+			+ ", hidden mostly in the demoscene, a subculture associated mainly with computer "
+			+ "hacking and digital graffiti. The MOD has formed a collaboration with the best "
+			+ "of these artists with the goal of adding a sense of spontaneity, improvisation, "
+			+ "and a live connection with an audience. The MOD satisfies this potential through "
+			+ "an all-star cast of musicians: Angelo Gregorio on saxophone, Damiano La Rocca on "
+			+ "guitar, Carl Corcoran on keyboards, Iënad Friedman on electric bass, "
+			+ "Wilfried Manzanza on drums."
+
+			,
 			isDefault: true,
 			pageTitle: 'The MOD: About',
 			navItem:
@@ -49,8 +63,8 @@ var TheModFakeStorage = function()
 	this.songs = 
 	[
 		{
-			UrlMP3: "mp3/Big Truffle in little China.mp3",
-			UrlOGG: null,
+			UrlMP3: "mp3/Big Truffle in Little China.mp3",
+			UrlOGG: "mp3/Big Truffle in Little China.ogg",
 			Title: "Big Truffle In Little China",// used as an ID.
 			CanBeOpeningSong: true,
 			//StartAt: 180,
@@ -58,7 +72,7 @@ var TheModFakeStorage = function()
 		},
 		{
 			UrlMP3: "mp3/If Only It Were You.mp3",
-			UrlOGG: null,
+			UrlOGG: "mp3/If Only It Were You.ogg",
 			Title: "If Only It Were You",
 			CanBeOpeningSong: true,
 			//StartAt: 180,
@@ -66,7 +80,7 @@ var TheModFakeStorage = function()
 		},
 		{
 			UrlMP3: "mp3/Go Anywhere.mp3",
-			UrlOGG: null,
+			UrlOGG: "mp3/Go Anywhere.ogg",
 			Title: "Go Anywhere",
 			CanBeOpeningSong: false,
 			//StartAt: 180,
