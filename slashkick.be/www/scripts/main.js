2f2a4479dae1f9aeb8ed1426d50f781f112dcb4e
tenfour <carlco@gmail.com>
2015-11-29 03:20:37 +0100

    pretty much ready for real action finally.

--- slashkick.be/www/scripts/main.js
+++ slashkick.be/www/scripts/main.js
@@ -11,15 +11,17 @@ var SlashKickApp = function()
 {
   this.startTime = Date.now();
   this.camera = new THREE.Camera();
-  this.camera.position.z = 1;
+  this.camera.position.z = 0;
 
   this.scene = new THREE.Scene();
 
-  var geometry = new THREE.PlaneBufferGeometry( 2, 1 );
+  var geometry = new THREE.PlaneBufferGeometry( 2, 2 );
 
   this.uniforms = {
     iGlobalTime: { type: "f", value: 1.0 },
-    iResolution: { type: "v3", value: new THREE.Vector3() }
+    iResolution: { type: "v3", value: new THREE.Vector3() },
+    iDate: { type: "v4", value: new THREE.Vector4() },
+    iMouse: { type: "v4", value: new THREE.Vector4() }
   };
 
   var material = new THREE.ShaderMaterial( {
@@ -43,10 +45,13 @@ var SlashKickApp = function()
 
 SlashKickApp.prototype.onWindowResize = function()
 {
-	this.uniforms.iResolution.value.x = window.innerWidth;
-	this.uniforms.iResolution.value.y = window.innerHeight;
+	var x = window.innerWidth;
+	var y = window.innerHeight;
+	this.uniforms.iResolution.value.x = x;
+	this.uniforms.iResolution.value.y = y;
+	//console.log("Resizing ("+x+","+y+")");
 	this.uniforms.iResolution.value.z = 0;
-	this.renderer.setSize( window.innerWidth, window.innerHeight );
+	this.renderer.setSize(x, y);
 };
 
 
@@ -61,6 +66,17 @@ SlashKickApp.prototype.drawScene = function()
 {
   var currentTime = Date.now();
   this.uniforms.iGlobalTime.value = (currentTime - this.startTime) * 0.001;
+  
+  this.uniforms.iDate.value.x = currentTime.year;
+  this.uniforms.iDate.value.y = currentTime.month;
+  this.uniforms.iDate.value.z = currentTime.day;
+  this.uniforms.iDate.value.w = currentTime.second + currentTime.milliseconds;
+  
+  this.uniforms.iMouse.value.x = 0;
+  this.uniforms.iMouse.value.y = 0;
+  this.uniforms.iMouse.value.z = 0;
+  this.uniforms.iMouse.value.w = 0;
+
   this.renderer.render( this.scene, this.camera );
 };
 
